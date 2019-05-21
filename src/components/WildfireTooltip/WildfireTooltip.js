30f1a0316a93af82dc779cc2fd3c7131b20bc33c
Nathan Griffiths <ngriffiths@postmedia.com>
2019-05-21 16:50:01 -0700

    updated tooltip data. still needs styling.

--- src/components/WildfireTooltip/WildfireTooltip.js
+++ src/components/WildfireTooltip/WildfireTooltip.js
@@ -7,13 +7,20 @@ const WildfireTooltip = (props) => {
 			<h3>Status: {props.data.FIRE_STATU}</h3>
 			<p>Location: {props.data.GEOGRAPHIC}</p>
 			<p>Size: {checkSize(props)} square kilometers.</p>
+			<p>Cause: {formatFireCause(props.data.FIRE_CAUSE)}</p>
+			<p>Started: {props.data.ignition_date}</p>
 		</Aux>
 	)
 };
 
+
 function checkSize(props) {
 	let size = (props.data.CURRENT_SI / 100).toFixed(2);
 	return size < 0.01 ? 'Spot fire. Less than 0.01' : size;
+}
+
+function formatFireCause(text) {
+	return text == '' ? 'Not available' : text;
 }
 
 export default WildfireTooltip;
