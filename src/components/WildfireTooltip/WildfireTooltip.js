8507962391cfbf4c0fb693584ce68521f6cb651b
Nathan Griffiths <ngriffiths@postmedia.com>
2019-05-22 14:14:18 -0700

    css styling for tooltip & adjustments to the summary text box to tighten up spacing

--- src/components/WildfireTooltip/WildfireTooltip.js
+++ src/components/WildfireTooltip/WildfireTooltip.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import Aux from '../Aux/Aux';
+import './WildfireTooltip.css';
 
 const WildfireTooltip = (props) => {
 	return(
-		<Aux>
-			<h3>Status: {props.data.FIRE_STATU}</h3>
-			<p>Location: {props.data.GEOGRAPHIC}</p>
-			<p>Size: {checkSize(props)} square kilometers.</p>
+		<div className="wf-tooltip">
+			<h3>{props.data.GEOGRAPHIC}</h3>
+			<p className={`status ${props.data.FIRE_STATU.toLowerCase().replace(/\s/g, '-')}`}>Status: {props.data.FIRE_STATU}</p>
+			<p>{`A fire that is currently ${checkSize(props)} square kilometres started on ${props.data.ignition_date}.`}</p>
+			
 			<p>Cause: {formatFireCause(props.data.FIRE_CAUSE)}</p>
-			<p>Started: {props.data.ignition_date}</p>
-		</Aux>
+		</div>
 	)
 };
 
-
 function checkSize(props) {
 	let size = (props.data.CURRENT_SI / 100).toFixed(2);
-	return size < 0.01 ? 'Spot fire. Less than 0.01' : size;
+	return size < 0.01 ? 'less than 0.01' : size;
 }
 
 function formatFireCause(text) {
