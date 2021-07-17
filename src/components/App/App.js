22567baca54ccb31982770a0c9e3dbb32473f034
Nathan Grifiths <njgriffiths@gmail.com>
2021-07-17 15:07:11 -0700

    remote file location changed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,16 +5,15 @@ import mapboxConfig from '../../data/mapbox-config';
 
 
 // Wildfires url
-const currentFiresURL = 'https://vs-postmedia-data.sfo2.digitaloceanspaces.com/wildfires.json';
+const currentFiresURL = 'https://vs-postmedia-data.sfo2.digitaloceanspaces.com/wildfires/wildfires.json';
 // Fires of note
-const firesOfNoteURL = 'https://vs-postmedia-data.sfo2.digitaloceanspaces.com/fon.json';
+const firesOfNoteURL = 'https://vs-postmedia-data.sfo2.digitaloceanspaces.com/wildfires/fon.json';
 // Fire perimeters
-const firePerimeters = 'https://vs-postmedia-data.sfo2.digitaloceanspaces.com/perimeters.json';
+const firePerimeters = 'https://vs-postmedia-data.sfo2.digitaloceanspaces.com/wildfires/perimeters.json';
 // evacuation order & alert perimeters
 const evacsAlertsUrl = 'https://services6.arcgis.com/ubm4tcTYICKBpist/arcgis/rest/services/Evacuation_Orders_and_Alerts/FeatureServer/0/query?f=pgeojson&where=1=1&outFields=ORDER_ALERT_STATUS,SHAPE__AREA,EVENT_TYPE';
 // firesmoke png file
 const fireSmokeUrl = 'https://vs-postmedia-data.sfo2.digitaloceanspaces.com/wildfires/fire-smoke-max.png';
-// const fireSmokeUrl = 'https://vs-postmedia-data.sfo2.digitaloceanspaces.com/wildfires/daily_maximum_20210714.png'
 
 // map tiles & attribution
 const mapboxStyle = 'https://api.maptiler.com/maps/outdoor/style.json?key=pRmETZ6APJE6l5kAzesF';
